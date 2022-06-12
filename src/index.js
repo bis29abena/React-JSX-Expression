6661f3ee7bc9c330c8dbddc74ca82cbe805b0b75
Bismark Osei <bismarkoseiabena@gmail.com>
2022-06-12 20:42:20 +0000

    new changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Bismark";
+const fname = "Bismark";
+const lname = "Osei";
 const luckyNumber = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {fname + " " + lname}!</h1>
     <p>Your Lucky number is {luckyNumber}</p>
   </div>,
   document.getElementById("root")
